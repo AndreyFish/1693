69c0ddd5d731625c0f2996bf19f94de887d1401b
Andrey <518fish@gmail.com>
2021-09-16 00:25:33 +0300

    Revert "test"
    This reverts commit 3dbd6499

--- task_5_8.py
+++
@@ -1,9 +0,0 @@
-def odd_nums(num):
-    # for num in range(1, num + 1, 2): # Список не чётных чисел,
-    # каждому последующему числу прибавляется 1 с шагом 2.
-    for num in range(0, num + 1, 2): # Список чётных чисел, если с нуля
-        yield num
-
-
-for i in odd_nums(15): # От 1 до 15
-    print(i)
