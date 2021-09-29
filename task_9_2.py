0d1f794f48729c72d7c685501faccdb967e1af32
Andrey <518fish@gmail.com>
2021-09-29 23:38:51 +0300

    Урок_9

---
+++ task_9_2.py
@@ -0,0 +1,15 @@
+
+
+
+
+class Road:
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+    def get_full_profit(self, weight=25, thickness=5):
+        return  f"{self._length} м * {self._width} м * {weight} кг * {thickness} см ="\
+            f"{(self._length * self._width * weight * thickness) / 1000} т"
+
+road_1 = Road(20, 5000)
+print(road_1.get_full_profit())
